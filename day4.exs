45a7e5a6808f71d08e4bf691c033b7dcf8c3b8e3
Stuart Osborne <stuart.osborne@infinera.com>
2019-12-03 20:57:10 +0100

    Start of day4

---
+++ day4.exs
@@ -0,0 +1,17 @@
+defmodule DayFour do
+
+end
+
+
+ExUnit.start()
+
+defmodule DayFourTest do
+    import DayFour
+    use ExUnit.Case
+
+    test "should do something" do
+        assert true == true
+    end
+end
+
+
