44edc3204a19a318bab7218f970e3d5734abfc71
Stuart Osborne <stuart.osborne@infinera.com>
2019-12-07 18:16:58 +0100

    WIP:day 4

--- day4.exs
+++ day4.exs
@@ -1,4 +1,41 @@
 defmodule DayFour do
+    
+    def load_data(file_name) do
+        File.read!(file_name)
+        |> String.trim
+        |> String.split("\n")
+        |> sort_records
+    end
+
+    def sort_records(records) do
+        records
+        |> Enum.sort
+    end
+
+    def parse_time_and_date(datestr) do
+        [date, time] = datestr |> String.split(" ")
+        parsed_date = String.split(date, "-") 
+                      |> Enum.reduce({},
+                          fn val, acc -> Tuple.append(acc, String.to_integer(val)) 
+                          end)
+        parsed_time = String.split(time, ":")
+                      |> Enum.reduce({},
+                          fn val, acc -> Tuple.append(acc, String.to_integer(val))
+                          end)
+        [parsed_date, parsed_time]
+    end
+
+    def parse_id_and_action(idaction) do
+        idaction
+    end
+
+    def parse_record(record) do
+        regex = ~r/\[(?<time>.+)\](?<action>.+)/
+        %{"time" => time, "action" => action} = Regex.named_captures(regex, record)
+        [d, t] = parse_time_and_date(time)
+        {d, t, action}
+    end
+
 
 end
 
@@ -9,8 +46,42 @@ defmodule DayFourTest do
     import DayFour
     use ExUnit.Case
 
-    test "should do something" do
-        assert true == true
+    setup do
+        data = [ "[1518-11-01 00:00] Guard #10 begins shift",
+                "[1518-11-01 00:05] falls asleep",
+                "[1518-11-01 00:25] wakes up",
+                "[1518-11-01 00:30] falls asleep",
+                "[1518-11-01 00:55] wakes up",
+                "[1518-11-01 23:58] Guard #99 begins shift",
+                "[1518-11-02 00:40] falls asleep",
+                "[1518-11-02 00:50] wakes up",
+                "[1518-11-03 00:05] Guard #10 begins shift",
+                "[1518-11-03 00:24] falls asleep",
+                "[1518-11-03 00:29] wakes up",
+                "[1518-11-04 00:02] Guard #99 begins shift",
+                "[1518-11-04 00:36] falls asleep",
+                "[1518-11-04 00:46] wakes up",
+                "[1518-11-05 00:03] Guard #99 begins shift",
+                "[1518-11-05 00:45] falls asleep",
+                "[1518-11-05 00:55] wakes up" ]
+        {:ok, data: data}
+    end
+
+    test "should sort list in time order", context do
+       assert sort_records(Enum.shuffle(context[:data])) == context[:data]
+    end
+
+    test "should parse time and date string" do
+        assert parse_time_and_date("1518-11-01 00:00") == [{1518, 11, 1}, {0, 0}]
+    end
+
+    test "should parse id and action" do
+        assert parse_id_and_action("Guard #10 begins shift") == {10, :begins}
+    end
+
+    test "should parse shift begin", context do
+        shift_start = context[:data] |> List.first
+        assert parse_record(shift_start) == {{1518,11,01}, {00,00}, 10, :begins}
     end
 end
 
