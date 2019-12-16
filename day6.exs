36a3772ab0b17221291a1c7dd04529a40d685f19
Stuart Osborne <stuart.osborne@infinera.com>
2019-12-16 20:40:43 +0100

    Day6 prepared.

---
+++ day6.exs
@@ -0,0 +1,17 @@
+defmodule DaySix do
+
+
+end
+
+
+ExUnit.start()
+
+defmodule DaySixTest do
+    import DaySix
+    use ExUnit.Case
+
+    test "should do something" do
+        assert true == true
+    end
+end
+
