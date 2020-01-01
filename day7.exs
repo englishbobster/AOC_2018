12d8cece9293971cc40be8099738ef543e8c5119
Stuart Osborne <stuart.osborne@infinera.com>
2020-01-01 19:50:42 +0100

    day7, parse line.

--- day7.exs
+++ day7.exs
@@ -1,5 +1,11 @@
 defmodule DaySeven do
-  
+    def parse_row(row) do
+        words = row |> String.split(" ")
+        first = Enum.at(words, 1)
+        next = Enum.at(words, 7)
+        {String.to_atom(first), String.to_atom(next)}
+    end
+
     def load_data(file_name) do
         File.read!(file_name)
         |> String.trim
@@ -14,8 +20,14 @@ defmodule DaySevenTest do
     import DaySeven
     use ExUnit.Case
 
-    test "should do something" do
-        assert true == true
+    setup do
+        data = [{:C, :A}, {:C, :F}, {:A, :B}, {:A, :D}, {:B, :E}, {:D, :E}, {:F, :E}]
+        {:ok, data: data}
+    end
+
+    test "should parse a line to tuple", context do
+        [head|_t] = context[:data]
+        assert parse_row("Step C must be finished before step A can begin.") == head
     end
 
 end
