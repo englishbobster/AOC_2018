d42a5c59bf92c22ca28eb3d8bd62bb70071cb462
Stuart Osborne <stuart.osborne@infinera.com>
2020-01-07 20:32:34 +0100

    start step ordering.

--- day7.exs
+++ day7.exs
@@ -12,6 +12,30 @@ defmodule DaySeven do
         |> String.split("\n")
     end
 
+    def create_dag(values) do
+        term_steps = values
+                      |> Enum.reduce([], fn {_x, y}, acc -> acc ++ [y] end)
+
+        65..90
+        |> Enum.map(fn code -> (String.Chars.to_string([code]) |> String.to_atom) end)
+        |> Enum.reduce(Keyword.new, fn x, acc -> 
+            steps = Keyword.get_values(values, x)
+            if not Enum.empty?(steps) do
+                Keyword.put_new(acc, x, steps) 
+            else
+                if Enum.member?(term_steps, x) do
+                    Keyword.put_new(acc, x, [])
+                else
+                    acc
+                end
+            end
+        end)
+    end
+
+    def order_steps(dag) do
+        []
+    end
+
 end
 
 ExUnit.start()
@@ -22,12 +46,23 @@ defmodule DaySevenTest do
 
     setup do
         data = [{:C, :A}, {:C, :F}, {:A, :B}, {:A, :D}, {:B, :E}, {:D, :E}, {:F, :E}]
-        {:ok, data: data}
+        dag = [C: [:A, :F], A: [:B, :D], B: [:E], D: [:E], F: [:E], E: []]
+        {:ok, data: data, dag: dag}
+    end
+
+    test "should order steps", context do
+        assert order_steps(context[:dag]) == [:C, :A, :B, :D, :F, :E]
     end
 
     test "should parse a line to tuple", context do
         [head|_t] = context[:data]
         assert parse_row("Step C must be finished before step A can begin.") == head
+    end
+
+    test "should create a dag", context do
+        actual_sorted = create_dag(context[:data]) |> Enum.sort
+        expected_sorted = context[:dag] |> Enum.sort
+        assert actual_sorted == expected_sorted
     end
 
 end
